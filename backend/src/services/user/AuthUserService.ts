119d8b9b33caf26aee4cdf8b983e439e25b883d1
Rafael <rafaelf.machado2004@gmail.com>
2022-12-13 12:50:02 -0300

    Logando e gerando token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from 'jsonwebtoken'
 
 interface AuthRequest{
     email: string;
@@ -24,7 +25,24 @@ class AuthUserService {
             throw new Error("User/password incorrect")
         }
 
-        return { ok: true }
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
