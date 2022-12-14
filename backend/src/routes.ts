fc952cc8e629662534cd42bcb899c64d94a8489e
Rafael <rafaelf.machado2004@gmail.com>
2022-12-14 14:09:16 -0300

    Cadastrando produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import multer from 'multer'
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
@@ -7,9 +8,15 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 router.post('/users', new CreateUserController().handle)
 
@@ -20,5 +27,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router };
