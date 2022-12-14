76669f83b525bd9019271f017c676cb4124ac9e6
Rafael <rafaelf.machado2004@gmail.com>
2022-12-14 17:37:43 -0300

    Criando e fechando um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,9 @@ import { ListCategoryController } from "./controllers/category/ListCategoryContr
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 import uploadConfig from './config/multer'
@@ -20,17 +23,16 @@ const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"))
 
 router.post('/users', new CreateUserController().handle)
-
 router.post('/session', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
-
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router };
