6d25a4301397867f40b523c07e308e0a24eeea96
Rafael <rafaelf.machado2004@gmail.com>
2022-12-15 14:05:30 -0300

    Listagem do pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,6 +17,7 @@ import { RemoveOrderController } from "./controllers/order/RemoveOrderController
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
+import { ListOrderController } from "./controllers/order/ListOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -42,5 +43,6 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
 
 export { router };
