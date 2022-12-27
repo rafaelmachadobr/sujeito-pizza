f920aaba220cdf5cd16d8c02ab3a3a9af5926691
Rafael <rafaelf.machado2004@gmail.com>
2022-12-27 12:06:41 -0300

    Componente preview

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,10 +1,33 @@
+import { useState, ChangeEvent } from "react";
 import Head from "next/head";
 import styles from "./styles.module.scss";
 import { Header } from "../../components/Header";
 
 import { canSSRAuth } from "../../utils/canSSRAuth";
 
+import { FiUpload } from "react-icons/fi";
+
 export default function Product() {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (!e) {
+      return;
+    }
+
+    const image = e.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === "image/png" || image.type === "image/jpeg") {
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+    }
+  }
+
   return (
     <>
       <Head>
@@ -17,6 +40,27 @@ export default function Product() {
           <h1>Novo produto</h1>
 
           <form className={styles.form}>
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#fff" />
+              </span>
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+
+              {avatarUrl && (
+                <img
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt="Foto do produto"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+
             <select>
               <option>Bebida</option>
               <option>Pizzas</option>
@@ -38,7 +82,9 @@ export default function Product() {
               className={styles.input}
             />
 
-            <button className={styles.buttonAdd} type="submit">Cadastrar</button>
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar
+            </button>
           </form>
         </main>
       </div>
